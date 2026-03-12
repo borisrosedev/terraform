fb846ea03e7ffcf2a1728af22117578e9be680e8
borisrosedev <boris.rose.dev@gmail.com>
2026-03-12 16:43:49 +0100

    feat/infra/v1

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-   region = var.aws_region
+  region = var.aws_region
 }
 
 
@@ -7,80 +7,84 @@ provider "aws" {
 
 data "aws_ami" "ubuntu" {
 
-  most_recent = true 
+  most_recent = true
 
   filter {
-    name = "name"
+    name   = "name"
     values = [var.ubuntu_ami_name]
   }
 
-    owners = [var.ubuntu_ami_owner]
+  owners = [var.ubuntu_ami_owner]
 }
 
 
 
 
 resource "aws_security_group" "boris_security_group" {
-    name = "boris_security_group"
-    vpc_id = var.vpc_id
+  name   = "boris_security_group"
+  vpc_id = var.vpc_id
 
 }
 
 
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-    security_group_id = aws_security_group.boris_security_group.id
-    from_port = "80"
-    to_port = "80"
-    ip_protocol = "http"
-    cidr_ipv4 = "0.0.0.0/0"
+  security_group_id = aws_security_group.boris_security_group.id
+  from_port         = "80"
+  to_port           = "80"
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 
 resource "aws_vpc_security_group_ingress_rule" "allow_icmp" {
-    ip_protocol = "icmp"
-    cidr_ipv4 = "0.0.0.0/0"
-    security_group_id = aws_security_group.boris_security_group.id
+  from_port         = "-1"
+  to_port           = "-1"
+  ip_protocol       = "icmp"
+  cidr_ipv4         = "0.0.0.0/0"
+  security_group_id = aws_security_group.boris_security_group.id
 }
 
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-    from_port = "22"
-    to_port = "22"
-    ip_protocol = "tcp"
-    cidr_ipv4 = "0.0.0.0/0"
-    security_group_id = aws_security_group.boris_security_group.id
+  from_port         = "22"
+  to_port           = "22"
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+  security_group_id = aws_security_group.boris_security_group.id
 }
 
 
 resource "aws_vpc_security_group_egress_rule" "allow_all" {
-    ip_protocol = "-1"
-    cidr_ipv4 = "0.0.0.0/0"
-    security_group_id = aws_instance.boris_security_group.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+  security_group_id = aws_security_group.boris_security_group.id
 }
 
 resource "aws_subnet" "boris_subnet" {
-    vpc_id = var.vpc_id
-    cidr_block = "172.31.20.0/24"
+  vpc_id     = var.vpc_id
+  cidr_block = "172.31.20.0/24"
 
 }
 
 resource "aws_key_pair" "boris_keypair" {
-   key_name = "boris_key"
-   public_key = var.my_public_key
+  key_name   = "boris_key"
+  public_key = file("~/.ssh/digital-school.pub")
 }
 
 
-resource "aws_instance" "boris_instance" {  
-    
-    security_groups = [ aws_security_group.boris_security_group.id ]
-    subnet_id = aws_subnet.boris_subnet.id
-    associate_public_ip_address = true
-    ami = data.aws_ami.ubuntu.id
+resource "aws_instance" "boris_instance" {
 
-    tags = {
-        Name = "boris_instance"
-    }
+  security_groups             = [aws_security_group.boris_security_group.id]
+  subnet_id                   = aws_subnet.boris_subnet.id
+  associate_public_ip_address = true
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.my_instance_type
+  key_name                    = aws_key_pair.boris_keypair.key_name
+
+  tags = {
+    Name = "boris_instance"
+  }
 }
 
 
--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 
 variable "ubuntu_ami_name" {
-    
-    description = "This is the ami name I use for my EC2 instance"
-    default = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"
+
+  description = "This is the ami name I use for my EC2 instance"
+  default     = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"
 
 }
 
 variable "ubuntu_ami_owner" {
 
-    default = "099720109477"
-  
+  default = "099720109477"
+
 }
 
 variable "vpc_id" {
-  default = "vpc-0ebcdb39f7a526ef9"    
+  default = "vpc-0ebcdb39f7a526ef9"
 }
 
 variable "vpc_ip_address" {
@@ -21,11 +21,13 @@ variable "vpc_ip_address" {
 }
 
 
-variable "aws_region" {  
+variable "aws_region" {
   default = "eu-west-3"
 }
 
 
-variable "my_public_key" {
-    default = file("~/.ssh/digital-school.pub")
+variable "my_instance_type" {
+  default = "t2.micro"
 }
+
+
